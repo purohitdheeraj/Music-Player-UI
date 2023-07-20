1cef9ec078df13fccb5889172de13a324e5de72b
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-07-20 19:11:41 +0530

    feat: add player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,31 @@
+import cover from "../assets/cover.png";
 function Player() {
-	return <section className="player">Player</section>;
+	return (
+		<section className="player">
+			<h2>Viva La Vida</h2>
+			<p>Coldplay</p>
+			<div className="player-cover">
+				<img
+					src={cover}
+					className="cover-image"
+					alt="cover-image"
+				/>
+			</div>
+
+			<input
+				type="range"
+				name="seek"
+				className="input input-seek-player"
+			/>
+			<div className="player-controls">
+				<button>more</button>
+				<button>prev</button>
+				<button>play</button>
+				<button>next</button>
+				<button>sound</button>
+			</div>
+		</section>
+	);
 }
 
 export default Player;
