583dcfa3940c6f5b34c161e89da63b5dd3fd578f
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-07-22 23:32:26 +0530

    feat: UI + prop types + responsive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Player from "./Player";
 import Sidebar from "./Sidebar";
 
 function Layout() {
+	const [showNavbar, setShowNavbar] = useState(true);
+
+	const toggleNavbar = () => {
+		setShowNavbar((prev) => !prev);
+	};
+
 	return (
 		<section className="layout">
-			<Navbar />
+			<Navbar
+				showNavbar={showNavbar}
+				toggleNavbar={toggleNavbar}
+			/>
 			<Sidebar />
 			<Player />
 		</section>
